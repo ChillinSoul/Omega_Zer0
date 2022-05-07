d413fd34544f0c331b329d1caad26dd55aae8616
ChillinSoul <99248410+ChillinSoul@users.noreply.github.com>
2022-05-07 18:35:29 +0200

    Update minmax.py
    fixed indexing mistake

--- Utils/functions/minmax.py
+++ Utils/functions/minmax.py
@@ -11,12 +11,13 @@ def MinMax(node:node,alpha = - maxsize,beta = maxsize):
 
     for child in node.chlidren:
         value = MinMax(child,child.c_depth,child.m_depth,alpha,beta)[0]
-        if (abs(maxsize*(-1)**(node.c_depth%2+2)-value)<abs(maxsize*(-1)**(node.c_depth%2+2)-best_move)):
-            best_move,_ = value 
+        if (abs(maxsize*(-1)**(node.c_depth%2+2)-value[0])<abs(maxsize*(-1)**(node.c_depth%2+2)-best_move)):
+            best_move,child.val = value 
+            
 
-            if node.c_depth%2: beta  = min(beta, value) # node.c_depth%2 is not 0 when the minimizing player is playing
-            else :             alpha = max(alpha,value) # the maximizer playe is playing
-            if beta<=alpha:    break                    # the best move has been found for the current player 
+            if node.c_depth%2: beta  = min(beta, value[0]) # node.c_depth%2 is not 0 when the minimizing player is playing
+            else :             alpha = max(alpha,value[0]) # the maximizer playe is playing
+            if beta<=alpha:    break                       # the best move has been found for the current player 
     
     
     return (best_move,child.move)
