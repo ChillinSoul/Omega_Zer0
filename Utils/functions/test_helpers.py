4336aff692ef2b9bfb3aef85404f0d4f901bba70
juuet <18198@ecam.be>
2022-05-07 22:15:29 +0200

    Pytest test_inbound OK

---
+++ Utils/functions/test_helpers.py
@@ -0,0 +1,8 @@
+import helpers as helpers
+import pytest
+
+
+def test_inbound():
+    
+       assert bool(helpers.inbound(4,-2))==True
+     
--- Utils/functions/unit_helpers.py
+++
@@ -1,8 +0,0 @@
-import Utils.functions.helpers as helpers
-import pytest
-
-
-def inbound_test():
-    
-       assert  helpers.inbound(4,-2)==False
-     
