41d9d833c2e8a7c357f900891b3f8d6f8a32a3d9
ChillinSoul <99248410+ChillinSoul@users.noreply.github.com>
2022-05-07 19:57:16 +0200

    helper file created
    created a new file containing helper functions
    
    moved functions from the node class to functions/helpers

---
+++ Utils/functions/helpers.py
@@ -0,0 +1,49 @@
+
+
+
+t = -8
+r = +1
+d = +8
+l = -1
+tl = t+l
+dl = d+l
+dr = d+r
+tr = t+r
+
+directions = [t,r,d,l,tl,dl,dr,tr]
+
+
+
+def inbound(move:int, dir:int):
+    if 0<=move+dir and move+dir <64:
+        if   move%8 == 0 and (dir !=-1 and dir !=-9 and dir !=  7):
+            return False
+        elif move%8 == 7 and (dir != 1 and dir != 9 and dir != -7):
+            return False
+        else:
+            return True
+    return False
+
+def Legal(board:list,move:int,player:str):
+    """
+    given a specific gamestate and a move, this function will return T/F depending if the move is legal.
+    """
+        
+    if player == "w": opp = "b"
+    else:             opp ="w"
+    if move == 0 or "":
+        for dir in directions:
+            i =1
+            while inbound(move,dir*(i)):
+                if board[move+dir] == opp:
+                    i+=1
+                elif board[move+dir] == player and i>1:
+                    return True
+    return False
+
+def Score_eval(self):
+    """
+    given a specific gamestate, this function will return the board score.
+    """
+    #(nb_w-nb_b)+(nb_w_c-nb_b_c)*0,5+(nb_w_e-nb_b_e)*0,25+(nb_w_d-nb_b_d)*(-0,5) si on et blanc, l'inverse si on est noir
+    return 1
