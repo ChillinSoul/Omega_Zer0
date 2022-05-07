3abcaf6d8d3ebc67557e903b90d5312b2f1599a5
ChillinSoul <99248410+ChillinSoul@users.noreply.github.com>
2022-05-07 14:28:45 +0200

    Update Node.py
    changed the spellin gof the class Node-> node

--- Utils/classes/Node.py
+++ Utils/classes/Node.py
@@ -1,12 +1,12 @@
 from sys import maxsize
 
-class Node(object):
+class node(object):
     """
     Node class:
     the node represents a gamestate,
-    it contains a current copy of the game, the current recusion depth (c_depth) and the maximum recurtion depth (m_depth).
-    the children attribute contains the possible futur gamestates at the c_depth +1 depth.
-    alpha and beta are arguments for the alpha-beta prunig alorithm that layers on to of minimax.
+    it contains a current copy of the game, the current recursion depth (c_depth) and the maximum recurtion depth (m_depth).
+    the children attribute contains the possible futur gamestates at the (c_depth +1) depth.
+    alpha and beta are arguments for the alpha-beta prunig alorithm that layers on top of minimax.
     
     """
     def __init__(self,c_depth:int,m_depth:int,board:list,val = 0,alpha = maxsize,beta = -maxsize):
@@ -32,12 +32,24 @@ class Node(object):
         self.MakeChild()# this line is what creates the entire tree.
         ######################### function(s)
     
+
+    def Legal(self):
+        """
+        given a specific gamestate and a move, this function will return T/F depending if the move is legal.
+        """
     def PGS_eval(self):
         """
         given a specific gamestate, this fuction uses the rules of the game to find all the possible gamestates
         """
+        return True
         pgs=[]
         return pgs
+    
+    def Score_eval(self):
+        """
+        given a specific gamestate, this function will return the board score.
+        """
+        return 1
 
 
     def MakeChild(self):
@@ -48,4 +60,4 @@ class Node(object):
             pgs = self.PGS_eval()
             for gs in pgs:
                 # creates a node at depth c_depth+1 whose board is one of the possible gamestates
-                self.chlidren.append(Node(self.c_depth+1,self.m_depth,gs,self.val,self.alpha,self.beta))
+                self.chlidren.append(node(self.c_depth+1,self.m_depth,gs,self.val,self.alpha,self.beta))
