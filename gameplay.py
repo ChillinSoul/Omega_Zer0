4ad23350e2f1f51eb97b36b7eec41dd187ddc0f2
juuet <18198@ecam.be>
2022-05-07 16:58:39 +0200

    Seperate message read in another file

--- gameplay.py
+++ gameplay.py
@@ -1,1 +1,11 @@
 def Choices(state):
+    #bestmob
+    #Meilleur move proposé par IA 
+    pass
+
+def state(message):
+    vie=message["lives"]
+    print("Il te reste",vie,"vies")
+    state=message["state"]
+    print(state)
+    Choices(state)
