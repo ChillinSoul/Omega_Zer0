d0f3507a0bb3088be142a8687ba6bf5eb8f98e17
juuet <18198@ecam.be>
2022-05-08 20:59:20 +0200

    First step Score_eval
    Best score

--- Utils/functions/helpers.py
+++ Utils/functions/helpers.py
@@ -1,6 +1,6 @@
 
 from copy import deepcopy
-from gameplay import state as state
+
 
 t = -8
 r = +1
@@ -60,13 +60,45 @@ def Legal(board:list,move:int,player:str,opp:str):
                     return True
     return False
 
-def Score_eval(self):
+def Score_eval(state):
     """
-    
+
     given a specific gamestate, this function will return the board score.
     """
-    boardinfo=state[3]
-    print(boardinfo)
+    #gameinfo=state['board']
+    #print("BOARD:",gameinfo)
+    gameinfo=[[45,46,23,21],[63,56,7,37]] #exemple
+    
+    corner=['63','56','7','0','07','00']
+
+    edge_=['63','56','7','0','07','00','15','23','31','39','45','01','02','03','04','05','06','07','08','09']
+    edge2=list(range(15,55,8))+list(range(57,62))+list(range(8,48,8))+list(range(1,6))
+    add2=["{:02d}".format(x) for x in edge2]
+    edge=add2+edge_
+
+    danger=[]
+    b=gameinfo[0]
+    w=gameinfo[1]
+    print(b,l)
+    blackdic={'black':b}
+    whitedic={'white':w}
+    #si on jour noir
+    #if b==nosPions:
+     #   for ele in w :
+      #      if any(ele in corner) for ele in j:
+    b_corner = set(b)&set(corner)
+    w_corner = set(w)&set(corner)
+    b_edge=set(b)&set(edge)
+    w_edge=set(b)&set(edge)
+    if w==nosPions: #si on est blanc, il faut importer la liste nosPions
+        (len(w)-len(b))+(len(w_corner)-len(b_corner))*0.5+(w_edge-b_edge)*0.25
+
+    if b==nosPions: #si on est noir
+        (len(b)-len(w))+(len(b_corner)-len(w_corner))*0.5+(b_edge-w_edge)*0.25
+
+
+
+
     #(nb_w-nb_b)+(nb_w_c-nb_b_c)*0,5+(nb_w_e-nb_b_e)*0,25+(nb_w_d-nb_b_d)*(-0,5) si on et blanc, l'inverse si on est noir
     return 1
 
