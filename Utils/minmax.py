3fe5e9c732b8128ffdba67712c155afaa8a08291
ChillinSoul <99248410+ChillinSoul@users.noreply.github.com>
2022-05-10 21:38:12 +0200

    alpha beta update
    fixed the algorithm

--- Utils/minmax.py
+++ Utils/minmax.py
@@ -6,7 +6,9 @@ def MinMax(node:node,alpha = - maxsize,beta = maxsize):
     best_move = maxsize*(-1)**(node.c_depth%2+1)
     best_child = node
     
-    if (node.c_depth == node.m_depth) or (abs(node.val)==maxsize): return node.Score_eval(),node.move 
+    if (node.c_depth == node.m_depth) or (abs(node.val)==maxsize):
+        #print(node.Score_eval())
+        return node.Score_eval(),node.move 
     
     
 
@@ -21,20 +23,21 @@ def MinMax(node:node,alpha = - maxsize,beta = maxsize):
 
             if node.c_depth%2: beta  = min(beta, value) # node.c_depth%2 is not 0 when the minimizing player is playing
             else :             alpha = max(alpha,value) # the maximizer playe is playing
-            if beta<=alpha:    break                       # the best move has been found for the current player 
+            if beta<=alpha:    print("");break                    # the best move has been found for the current player 
     
-    
+        #print(alpha,beta)
     return [best_move,best_child.move]
 
 start = time()
-list_b=[28,35]
-list_w=[27,36]
+list_b=[19,27,28,35,36,37,42,45]
+list_w=[18,20,21,34,38,43,44,49,11]
 lst = [0 for _ in range(64)]
 for i in list_b:
     lst[i] = "b"
 for j in list_w:
     lst[j] = "w"
-bob = node(0,8,lst,"w")
+bob = node(0,4,lst,"w")
 print(time()-start)
 print(len(bob.chlidren))
 print(MinMax(bob))
+print(time()-start)
