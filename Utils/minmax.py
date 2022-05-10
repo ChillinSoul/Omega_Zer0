dea15b4270e4fdb9549e11ba10876525583e3f78
ChillinSoul <99248410+ChillinSoul@users.noreply.github.com>
2022-05-10 21:01:14 +0200

    AI makes sense
    fixed the play detection

--- Utils/minmax.py
+++ Utils/minmax.py
@@ -3,17 +3,21 @@ from Node import node
 from time import time
 
 def MinMax(node:node,alpha = - maxsize,beta = maxsize):
-
+    best_move = maxsize*(-1)**(node.c_depth%2+1)
+    best_child = node
+    
     if (node.c_depth == node.m_depth) or (abs(node.val)==maxsize): return node.Score_eval(),node.move 
     
-    best_move = maxsize*(-1)**(node.c_depth%2+1)
-    best_child = None
+    
 
     for child in node.chlidren:
         value,_ = MinMax(child,alpha,beta)
-        if (abs(maxsize*(-1)**(node.c_depth%2+2)-value)<abs(maxsize*(-1)**(node.c_depth%2+2)-best_move)):
+        
+        if (abs(maxsize*(-1)**(child.c_depth%2+1)-value)<abs(maxsize*(-1)**(child.c_depth%2+1)-best_move)):
+            
             best_move,best_child = value,child 
             
+
 
             if node.c_depth%2: beta  = min(beta, value) # node.c_depth%2 is not 0 when the minimizing player is playing
             else :             alpha = max(alpha,value) # the maximizer playe is playing
@@ -30,7 +34,7 @@ for i in list_b:
     lst[i] = "b"
 for j in list_w:
     lst[j] = "w"
-bob = node(0,4,lst,"w")
+bob = node(0,8,lst,"w")
 print(time()-start)
 print(len(bob.chlidren))
 print(MinMax(bob))
