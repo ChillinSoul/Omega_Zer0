5c01b057e1dde69833b8d588456550f74a9f1658
juuet <18198@ecam.be>
2022-05-11 14:50:52 +0200

    Update gameplay

--- gameplay.py
+++ gameplay.py
@@ -1,6 +1,7 @@
 import json
 import Utils.functions.helpers as helpers
 import Utils.minmax as minmax
+from Utils.Node import node as node
 
 import copy
 
@@ -21,8 +22,17 @@ def Info(message,client):
             boardinfo= message['state']['state'[3]]
             if len(boardinfo[0]+boardinfo[1])==4:
                 player='b'
+                list_b=boardinfo[0]
+                opp='w'
+                list_w=boardinfo[1]
+                lst = [0 for _ in range(64)]
+                for i in list_b:
+                    lst[i] = "b"
+                for j in list_w:
+                    lst[j] = "w"
+                
                 boardpawns=[boardinfo[0]+boardinfo[1]]
-                bestmove=minmax.MinMax(boardpawns,player)
+                bestmove=minmax.MinMax(bob)
                 move_answ=json.dumps({
                     "response": "move",
                     "move":bestmove,
@@ -31,9 +41,24 @@ def Info(message,client):
                 c=1
                 break
             if len(boardinfo[0]+boardinfo[1])==5:
-                player='w'
+                if len(boardinfo[0])==3:
+                    player='w'
+                    list_w=boardinfo[1]
+                    opp='b'
+                    list_b=boardinfo[0]
+                if len(boardinfo[1]==3):
+                    player='w'
+                    list_w=boardinfo[0]
+                    opp='b'
+                    list_b=boardinfo[1]
+                    lst = [0 for _ in range(64)]
+                for i in list_b:
+                    lst[i] = "b"
+                for j in list_w:
+                    lst[j] = "w"
+                bob = node(0,4,lst,"w")
                 boardpawns=[boardinfo[0]+boardinfo[1]]
-                bestmove=minmax.MinMax(boardpawns,player)
+                bestmove=minmax.MinMax(bob)
                 move_answ=json.dumps({
                     "response": "move",
                     "move":bestmove,
@@ -43,8 +68,22 @@ def Info(message,client):
                 break
 
             boardinfo= message['state']['state'[3]]
+
+            list_b=[19,27,28,35,36,37,42,45]
+            list_w=[18,20,21,34,38,43,44,49,11]
+            lst = [0 for _ in range(64)]
+            for i in list_b:
+                lst[i] = "b"
+            for j in list_w:
+                lst[j] = "w"
+            bob = node(0,4,lst,"w")
+            
+            print(len(bob.chlidren))
+            print(minmax.MinMax(bob))
+            
+
             boardpawns=[boardinfo[0]+boardinfo[1]]
-            bestmove=minmax.MinMax(boardpawns,player)
+            bestmove=minmax.MinMax(bob)
             move_answ=json.dumps({
                     "response": "move",
                     "move":bestmove,
