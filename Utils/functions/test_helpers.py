d1ed241ca5ba0714916dd06d862c7448e78f67cf
juuet <18198@ecam.be>
2022-05-11 14:04:37 +0200

    Uptade test

--- Utils/functions/test_helpers.py
+++ Utils/functions/test_helpers.py
@@ -6,16 +6,18 @@ def test_inbound():
        
        assert bool(helpers.inbound(4,-2))==True
        pass
-       assert bool(helpers.inbound(7,1))==True
+       assert bool(helpers.inbound(7,1))==False
        pass
        assert bool(helpers.inbound(7,-8))==False
        pass
 
 def test_Legal():
-       b=[4,5]
-       op='37' 
-       i=1
-       if i>1:
-              assert bool(helpers.Legal(b,36,'player',op))==True
+       
+      
+       b=[0:63]
+       op='w' 
+       player='b'
+      
+       assert bool(helpers.Legal(b,36,player,op))==False
 
      
