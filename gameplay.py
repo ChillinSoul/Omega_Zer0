3664d01c14368b564976e599464f88ba27e75a24
juuet <99720443+juuet@users.noreply.github.com>
2022-05-12 08:12:01 +0200

    Update gameplay

--- gameplay.py
+++ gameplay.py
@@ -2,17 +2,26 @@ import json
 import Utils.helpers as helpers
 import Utils.minmax as minmax
 from Utils.Node import node as node
-
+t=0
+player=""
 def Info(message,client):
     state=message['state']
-
-    if state[player]:
-        player = "w"
-    else:
-        player = "b"
-    
-    
+    global player
+    global t
     boardinfo= message['state']['board']
+    if t<1:
+       if len(boardinfo[0]+boardinfo[1])==2:
+           player= "b"
+       else:
+           player="w"
+       t=1
+   
+
+
+
+    
+    
+    
 
 
 
@@ -27,16 +36,19 @@ def Info(message,client):
     object,bestmove=minmax.MinMax(AI)
 
     if bestmove == None:
+        t=0
         giveup=json.dumps({
         "response": "giveup",
         })
         client.send(giveup.encode())
-    else:        
+    else: 
+
         move_answ=json.dumps({
         "response": "move",
         "move":bestmove,
         "message": "Mouahah"})
         client.send(move_answ.encode())
+    return t,player
 
 
 
